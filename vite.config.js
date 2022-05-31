6c411d800f2d70a7e014d4962fc99bf9f9821832
Dungnm2408 <dunghung2408@gmail.com>
2022-06-01 00:53:24 +0700

    tsst

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,8 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        detailProduct: resolve(__dirname, 'detail.html'),
-        products: resolve(__dirname, 'products.html'),
-        index1: resolve(__dirname, 'index1.html'),
-
+        main: resolve(__dirname, 'index1.html'),
+        
         index2: resolve(__dirname, 'index2.html'),
 
         index3: resolve(__dirname, 'index3.html'),
