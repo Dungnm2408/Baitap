684d25fda448db5697c0e6f345fc1596e9d7715c
Dungnm2408 <dunghung2408@gmail.com>
2022-06-01 00:49:40 +0700

    Bài Lap

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,16 @@ module.exports = defineConfig({
         main: resolve(__dirname, 'index.html'),
         detailProduct: resolve(__dirname, 'detail.html'),
         products: resolve(__dirname, 'products.html'),
+        index1: resolve(__dirname, 'index1.html'),
+
+        index2: resolve(__dirname, 'index2.html'),
+
+        index3: resolve(__dirname, 'index3.html'),
+
+        index4: resolve(__dirname, 'index4.html'),
+        
+        index5: resolve(__dirname, 'index5.html'),
+
 
       }
     }
